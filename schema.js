7c70aa9e3d1b442b762131019dde307fd6335e0a
Tim Malstead <timmmalstead@gmail.com>
2020-01-06 11:22:38 -0800

    cleans files, preps for front end development

--- schema.js
+++ schema.js
@@ -1,6 +1,4 @@
-// const makeExecutableSchema = require("graphql-tools")
 import { makeExecutableSchema } from "graphql-tools"
-// const resolvers = require("./resolvers")
 import { resolvers } from "./resolvers"
 
 const typeDefs = `
