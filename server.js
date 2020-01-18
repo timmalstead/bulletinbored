2c3a1ac53d04aa43c5b031cac0df926534c4b7e1
Tim Malstead <timmmalstead@gmail.com>
2020-01-18 14:33:47 -0800

    preps for heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import cors from "cors"
 import express from "express"
 import graphqlHttp from "express-graphql"
+import path from "path"
 import schema from "./schema"
 
 const app = express()
@@ -13,5 +14,9 @@ require("./db/db")
 app.use(cors())
 
 app.use("/graphql", graphqlHttp({ schema: schema, graphiql: true }))
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"))
+})
 
 app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`))
