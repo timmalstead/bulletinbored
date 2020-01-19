fcb6902ced6cd951c150f60104d1e9eacdc3da04
Tim Malstead <timmmalstead@gmail.com>
2020-01-19 10:04:59 -0800

    REALLY fixes port issue

--- src/index.js
+++ src/index.js
@@ -11,15 +11,11 @@ import { InMemoryCache } from "apollo-cache-inmemory"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
 
-let httpLink
+const httpLink = createHttpLink({
+  uri: "https://bulletinbored.herokuapp.com/graphql"
+})
 
-if (process.env.NODE_ENV === "development") {
-  httpLink = createHttpLink({
-    uri: "https://bulletinbored.herokuapp.com/graphql"
-  })
-} else {
-  httpLink = createHttpLink({ uri: "http://localhost:8000/graphql" })
-}
+// const httpLink = createHttpLink({ uri: "http://localhost:8000/graphql" })
 
 const link = ApolloLink.from([httpLink])
 
